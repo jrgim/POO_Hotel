758cb2b2d5358a63bddc0b655d3dfe42dd58b786
Jorge <103746923+jrgim@users.noreply.github.com>
2023-12-26 12:33:50 +0100

    Actualizar constructores Reserva.cpp

--- Hotel/Reserva.cpp
+++ Hotel/Reserva.cpp
@@ -1,1 +1,13 @@
 #include "Reserva.h"
+Reserva::Reserva(){
+    Clientes = new Cliente[numClientes];
+    numNoches=0;
+    fechaEntrada = "10/12/2023";
+    numHabitacion= 0;
+}
+
+Reserva::Reserva(int numNoches, string fechaEntrada, int numHabitacion){
+    this->numNoches = numNoches;
+    this->fechaEntrada = fechaEntrada;
+    this->numHabitacion=numHabitacion;
+}
