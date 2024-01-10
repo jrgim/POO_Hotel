1c7b4292b397c25fa8f3f9919b1d2977cc007032
Jorge <103746923+jrgim@users.noreply.github.com>
2024-01-10 22:34:17 +0100

    Colores en la terminal

--- Hotel/Main.cpp
+++ Hotel/Main.cpp
@@ -1,16 +1,42 @@
 #include<iostream>
+#include<Windows.h>
 #include "Hotel.h"
+#define color SetConsoleTextAttribute
 
 int main() {
-	int opcionSeleccionada = 0;
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	int opcionSeleccionada = 0, selecionNum=-1;
 	string aux;
 	Cliente clientAux;
 	Hotel hotel;
+
 	while (opcionSeleccionada != 11) {
-		cout << "~~~~~~~~~~~~Menu Recepcionista hotel~~~~~~~~~~~~\n" << endl
-			<< "1. Consultar precio de una habitacion\n2. Consultar descuento\n3. Modificar descuento para clientes habituales" << endl
-			<< "4. Cambiar precio habitacion\n5. Mostrar precio de una reserva\n6. Reservar Habitacion\n7. Calcular ganancias de un mes" << endl
-			<< "8. Lista de las habitaciones\n9. Guardar\n10. Cargar datos\n11. Salir" << endl << "Seleccione una opcion: ";
+		color(hConsole, 1);
+		cout << "~~~~~~~~~~~~Menu Recepcionista hotel~~~~~~~~~~~~\n" << endl;
+		color(hConsole, 2);
+		cout << "1. Consultar precio de una habitacion\n";
+		color(hConsole, 3);
+		cout << "2. Consultar descuento\n";
+		color(hConsole, 5);
+		cout << "3. Modificar descuento para clientes habituales" << endl;
+		color(hConsole, 6);
+		cout << "4. Cambiar precio habitacion\n";
+		color(hConsole, 7);
+		cout << "5. Mostrar precio de una reserva\n";
+		color(hConsole, 9);
+		cout << "6. Reservar Habitacion\n";
+		color(hConsole, 10);
+		cout << "7. Calcular ganancias de un mes" << endl;
+		color(hConsole, 1);
+		cout << "8. Lista de las habitaciones\n";
+		color(hConsole, 2);
+		cout << "9. Guardar\n";
+		color(hConsole, 3);
+		cout << "10. Cargar datos\n";
+		color(hConsole, 4);
+		cout << "11. Salir" << endl;
+		color(hConsole, 3);
+		cout << "Seleccione una opcion: ";
 		cin >> opcionSeleccionada;
 		switch (opcionSeleccionada)
 		{
@@ -49,6 +75,13 @@ int main() {
 			hotel.cargar();
 			break;
 		case 11:
+			cout << "\nQueres guardar los cambios antes de cerrar:0. NO / 1. SI: ";
+			cin >> selecionNum;
+			if (selecionNum == 1) {
+				hotel.guardar();
+				cout << "\nDATOS GUARDADOS";
+			}
+			else cout << "\nDATOS BORRADOS\n";
 			break;
 		default:
 			cout << "Numero seleccionado erroneo." << endl;
